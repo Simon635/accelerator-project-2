541ed70bf802168a3a5df6554e69c4f0bd36fdc9
simon <semyon.dudin@inbox.ru>
2025-04-27 17:36:09 +0300

    готовый блок training

--- source/js/swiper-init.js
+++ source/js/swiper-init.js
@@ -3,27 +3,25 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 
 function toggleNavButtons(swiper) {
-  const prevButton = document.querySelector('.slider-navigation--prev');
-  const nextButton = document.querySelector('.slider-navigation--next');
+  const sliderWrapper = swiper.el.closest('.tours, .training, .reviews, .galary');
+  const prevButton = sliderWrapper?.querySelector('.slider-navigation--prev, .reviews__button--prev, .galary__slider-button--prev');
+  const nextButton = sliderWrapper?.querySelector('.slider-navigation--next, .reviews__button--next, .galary__slider-button--next');
 
   if (!prevButton || !nextButton) {
     return;
   }
 
-  const totalSlides = swiper.slides.length;
-  const slidesPerView = swiper.params.slidesPerView;
-  const currentIndex = swiper.activeIndex;
-
   prevButton.disabled = swiper.isBeginning;
-  nextButton.disabled = currentIndex >= totalSlides - slidesPerView;
+  nextButton.disabled = swiper.isEnd;
 }
 
 export const initSlider = () => {
   document.addEventListener('DOMContentLoaded', () => {
-    // Инициализация слайдера для hero__swiper
+    // Hero slider
     new Swiper('.hero__swiper', {
       loop: true,
       slidesPerView: 1,
+      initialSlide: 0,
       pagination: {
         el: '.hero__swiper-pagination',
         bulletClass: 'hero__pagination-control',
@@ -38,14 +36,14 @@ export const initSlider = () => {
       }
     });
 
-    // Инициализация слайдера для tours__swiper
+    // Tours slider
     new Swiper('.tours__swiper', {
       loop: false,
       slidesPerView: 1,
       slidesPerGroup: 1,
       navigation: {
-        nextEl: '.slider-navigation--next',
-        prevEl: '.slider-navigation--prev',
+        nextEl: '.tours__slider-button--next',
+        prevEl: '.tours__slider-button--prev',
       },
       breakpoints: {
         768: {
@@ -55,6 +53,7 @@ export const initSlider = () => {
         1440: {
           slidesPerView: 3,
           spaceBetween: 30,
+          allowTouchMove: false,
         }
       },
       on: {
@@ -66,5 +65,101 @@ export const initSlider = () => {
         },
       }
     });
+
+    // Training slider
+    new Swiper('.training__swiper', {
+      loop: false,
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      initialSlide: 2,
+      navigation: {
+        nextEl: '.training__slider-button--next',
+        prevEl: '.training__slider-button--prev',
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+          initialSlide: 0,
+        },
+        1440: {
+          slidesPerView: 4,
+          spaceBetween: 20,
+          allowTouchMove: false,
+          initialSlide: 0,
+        }
+      },
+      on: {
+        init: function () {
+          toggleNavButtons(this);
+        },
+        slideChange: function () {
+          toggleNavButtons(this);
+        },
+      }
+    });
+
+    // Reviews slider
+    new Swiper('.reviews__slider', {
+      loop: false,
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 40,
+      centeredSlides: false,
+      navigation: {
+        nextEl: '.reviews__slider-button--next',
+        prevEl: '.reviews__slider-button--prev',
+      },
+      breakpoints: {
+        768: {
+          spaceBetween: 30,
+        },
+        1440: {
+          spaceBetween: 120,
+          allowTouchMove: false,
+        }
+      },
+      on: {
+        init: function () {
+          toggleNavButtons(this);
+        },
+        slideChange: function () {
+          toggleNavButtons(this);
+        },
+      }
+    });
+
+    // Galary slider
+    new Swiper('.galary__slider', {
+      loop: true,
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+      spaceBetween: 5,
+      navigation: {
+        nextEl: '.galary__slider-button--next',
+        prevEl: '.galary__slider-button--prev',
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        1440: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+          allowTouchMove: false,
+        }
+      },
+      on: {
+        init: function () {
+          toggleNavButtons(this);
+        },
+        slideChange: function () {
+          toggleNavButtons(this);
+        },
+      }
+    });
+
   });
 };
+
